ef0cc6b5e654e7cbfb019df66c83babeef57af5d
Leonardo Edelman Wajnsztok <LeoWajnsztok@MacBook-Pro-de-Leonardo.local>
2015-03-12 13:10:37 -0300

    Initial Commit

---
+++ WakerCentral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WakerCentral
+//
+//  Created by Leonardo Edelman Wajnsztok on 12/03/15.
+//  Copyright (c) 2015 Leonardo Edelman Wajnsztok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
