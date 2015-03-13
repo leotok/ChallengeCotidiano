11807f5197b5e94a6055d9195faf28b687cb81b3
LucasMW <great-lucas@hotmail.com>
2015-03-13 16:05:10 -0300

    Choose sound and vibration

--- WakerCentral/ByThisTimeController.swift
+++ WakerCentral/ByThisTimeController.swift
@@ -11,12 +11,25 @@ import AVFoundation
 
 class ByThisTimeController: UIViewController {
     
-    var timeCounter: NSTimer = NSTimer()
-    var waitTime: NSTimeInterval = NSTimeInterval()
+    private var timeCounter: NSTimer = NSTimer()
+    private var waitTime: NSTimeInterval = NSTimeInterval()
+    private var player:AVAudioPlayer = AVAudioPlayer();
+    private var vibrate: Bool = true
+    private var sound: Bool = false
+    
+    
+    
+    
+
+    
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         timeCounter = NSTimer.scheduledTimerWithTimeInterval(waitTime, target: self, selector: Selector("waitIsOver"), userInfo: nil, repeats: true)
+        stopButton.enabled=false;
+        
+      
         
         println("WaitTime: \(waitTime)")
         
@@ -30,11 +43,60 @@ class ByThisTimeController: UIViewController {
     }
     func waitIsOver()
     {
+        stopButton.enabled=true;
+        
+        
         println("TheWaitIsOver")
-        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate)) //vibrates the phone
+        if(vibrate==true)
+        {
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate)) //vibrates the phone
+        }
+        if(sound==true)
+        {
+            player.play();
+            
+        }
+        
     }
     
-    
+    @IBAction func StopButtonPressed(sender: UIButton)
+    {
+        timeCounter.invalidate()
+        player.stop()
+        self.stopButton.enabled=false;
+        
+        
+    }
+    func setSoundToPlay(soundName:String, ofType:String, timeToWait: NSTimeInterval)
+    {
+        waitTime=timeToWait
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(soundName, ofType: ofType)!)
+        var error:NSError?
+        player = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        player.prepareToPlay()
+        sound=true
+        vibrate=false
+        
+    }
+    func setToVibrate(timeToWait: NSTimeInterval)
+    {
+        waitTime=timeToWait
+        sound=false
+        vibrate=true
+        
+    }
+    func setToVibrateAndToPlaySound(soundName:String, ofType:String, timeToWait: NSTimeInterval)
+    {
+        waitTime=timeToWait
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(soundName, ofType: ofType)!)
+        var error:NSError?
+        player = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        player.prepareToPlay()
+        sound=true
+        vibrate=true
+        
+        
+    }
 
     /*
     // MARK: - Navigation
