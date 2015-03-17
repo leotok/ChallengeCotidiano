7d986d46a6e78ec3b461a4341d8bc8ac9cd6912b
LucasMW <great-lucas@hotmail.com>
2015-03-17 11:59:55 -0300

    Started development of SetMode

---
+++ WakerCentral/SetModeVController.swift
@@ -0,0 +1,35 @@
+//
+//  SetModeVController.swift
+//  WakerCentral
+//
+//  Created by Lucas Menezes on 17/03/15.
+//  Copyright (c) 2015 Leonardo Edelman Wajnsztok. All rights reserved.
+//
+
+import UIKit
+
+class SetModeVController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
