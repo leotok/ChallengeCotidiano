a8180800e807c1f4ecf539dc3a637c52fee22720
LucasMW <great-lucas@hotmail.com>
2015-03-17 18:38:08 -0300

    Interface of SetMode communicates with BythisTimeVcontroller

--- WakerCentralTests/SetModeVController.swift
+++ WakerCentralTests/SetModeVController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SetModeVController: UIViewController {
+class SetModeVController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var vibrateSwitch: UISwitch!
     @IBOutlet weak var soundSwitch: UISwitch!
@@ -20,23 +20,25 @@ class SetModeVController: UIViewController {
     var soundName: String = String("")
     var soundFormat: String = String("mp3")
     
+    var soundArray: [String] = ["alarm_classic","alarm_sound","alarm"]
+    
     @IBOutlet weak var okButton: UIButton!
     override func viewDidLoad()
     {
         super.viewDidLoad()
         //Default
         
-        vibrateSwitch.selected=true
-        soundSwitch.selected=false
+        vibrateSwitch.on=true
+        soundSwitch.on=false
         
-        if(soundSwitch.selected==false)
+        if(soundSwitch.on==false)
         {
             soundPicker.hidden=true
         }
         
-        tapSwitch.selected=true
-        slideSwitch.selected=false
-        
+        tapSwitch.on=true
+        slideSwitch.on=false
+        soundPicker.delegate=self
         
         
         
@@ -44,6 +46,17 @@ class SetModeVController: UIViewController {
     }
 
 
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+        var label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        label.text = soundArray[row]
+        
+        return label    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,10 +65,12 @@ class SetModeVController: UIViewController {
     @IBAction func okButtonPressed(sender: UIButton) {
         let vc = ByThisTimeVController(nibName: "ByThisTimeVController", bundle: nil)
         
-        var timeToWait: NSTimeInterval = 1
-        if(vibrateSwitch.selected==true)
+        
+        var timeToWait: NSTimeInterval = timePicker.countDownDuration
+        println("on \(vibrateSwitch.on) \(soundSwitch.on)")
+        if(vibrateSwitch.on==true)
         {
-            if(soundSwitch.selected==true)
+            if(soundSwitch.on==true)
             {
                 vc.setToVibrateAndToPlaySound("alarm_sound", ofType: ".mp3", timeToWait: timeToWait)
             }
@@ -66,7 +81,7 @@ class SetModeVController: UIViewController {
         }
         else
         {
-            if(soundSwitch.selected==true)
+            if(soundSwitch.on==true)
             {
                 vc.setSoundToPlay("alarm_sound", ofType: ".mp3", timeToWait: timeToWait)
             }
@@ -80,6 +95,9 @@ class SetModeVController: UIViewController {
         self.presentViewController(vc, animated: false, completion: nil)
     }
 
+    @IBAction func soundSwitchChanged(sender: UISwitch) {
+       soundPicker.hidden = !sender.on
+    }
     /*
     // MARK: - Navigation
 
