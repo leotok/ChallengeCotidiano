6666e5acc9e5094ad2dae7362df6a6d1c93ef06b
LucasMW <great-lucas@hotmail.com>
2015-03-24 18:54:02 -0300

    Started Map implementation and backgrond

--- WakerCentral/NearLocationVController.swift
+++ WakerCentral/NearLocationVController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class NearLocationVController: UIViewController
+class NearLocationVController: UIViewController,CLLocationManagerDelegate
 {
 
     var presentPlace: String = String();
     var destination: String = String();
+    var timeToWait: NSTimeInterval = NSTimeInterval();
+    let locationManager = CLLocationManager()
     
     
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        let location = CLLocationCoordinate2D(
+            latitude: 51.50007773,
+            longitude: -0.1246402
+        )
+        // 2
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
         // Do any additional setup after loading the view.
     }
 
