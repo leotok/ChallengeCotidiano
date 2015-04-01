f73bbfdf2dea3f9766308d9d9087592371159faf
andreiarsa <andreiarsa@icloud.com>
2015-04-01 13:34:29 -0300

    Add back button

--- WakerCentral/AlarmListVController.swift
+++ WakerCentral/AlarmListVController.swift
@@ -13,12 +13,42 @@ class AlarmListVController: UITableViewController
     
     var dao : DAO = DAO()
     var alarms : [Alarm] = []
-        
+    
+    var backBt: UIButton = UIButton(frame: CGRectMake(15 ,27 , 30 , 30))
+    var plusBt: UIButton = UIButton (frame: CGRectMake(280, 27, 30, 30))
+
+
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.alarms = dao.getAlarms()
+        
+        
+        backBt.setImage(UIImage(named: "backButton"), forState: UIControlState.Normal)
+        backBt.addTarget(self, action: Selector("backConfig:"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        plusBt.setImage(UIImage(named: "maisButton"), forState: UIControlState.Normal)
+        plusBt.addTarget(self, action: Selector("backSave:"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
+        //Add o elememento na tela
+        
+        view.addSubview(backBt)
+        view.addSubview(plusBt)
+        
     }
+    
+    @IBAction func backSave(sender: UIButton)
+    {
+    }
+    
+    @IBAction func backConfig(sender: UIButton)
+    {
+        self.dismissViewControllerAnimated(false, completion: nil)
+        
+    }
+    
 
     override func didReceiveMemoryWarning()
     {
@@ -76,58 +106,5 @@ class AlarmListVController: UITableViewController
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool 
-    {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) 
-    {
-        if editingStyle == .Delete
-        {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } 
-        else if editingStyle == .Insert
-        {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) 
-    {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool 
-    {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) 
-    {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
